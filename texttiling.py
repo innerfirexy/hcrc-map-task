7abec0cd5da2c06d3d1b1d038510a00f759b83de
Yang Xu <innerfirexy@gmail.com>
2016-02-10 14:42:08 -0500

    to run texttiling.py

--- texttiling.py
+++ texttiling.py
@@ -12,7 +12,7 @@ def db_conn(db_name):
     # db init: ssh yvx5085@brain.ist.psu.edu -i ~/.ssh/id_rsa -L 1234:localhost:3306
     conn = MySQLdb.connect(host = "127.0.0.1",
                     user = "yang",
-                    port = 1234,
+                    port = 3306,
                     passwd = "05012014",
                     db = db_name)
     return conn
@@ -37,18 +37,19 @@ def texttiling():
         except Exception as e:
             raise e
         else:
-            utter_id = 1
+            i = 0
             for j, seg in enumerate(segmented_text):
                 topic_id = j+1
-                sents = [s for s in seg.split('\n\n\n\t')]
+                sents = [s for s in seg.split('\n\n\n\t') if s != '']
                 for k, s in enumerate(sents):
                     in_topic_id = k+1
                     sql = 'UPDATE utterances SET topicID = %s, inTopicID = %s \
                         WHERE observation = %s AND utterID = %s'
-                    cur.execute(sql, (topic_id, in_topic_id, obsv, utter_id))
-                    utter_id += 1
+                    cur.execute(sql, (topic_id, in_topic_id, obsv, utter_id[i]))
+                    i += 1
                     conn.commit()
-            sys.stdout.write('\r{}/{}'.format(i+1, len()))
+            sys.stdout.write('\r{}/{}'.format(i+1, len(unique_observs)))
+            sys.stdout.flush()
 
 # main
 if __name__ == '__main__':
