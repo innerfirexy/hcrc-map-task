ab6c4eb3cfa1bda93f59419470ef3323f046182c
Yang Xu <innerfirexy@gmail.com>
2016-02-10 19:32:03 -0500

    add ploting

--- analysis.r
+++ analysis.r
@@ -9,7 +9,7 @@ library(ggplot2)
 
 # ssh yvx5085@brain.ist.psu.edu -i ~/.ssh/id_rsa -L 1234:localhost:3306
 conn = dbConnect(MySQL(), host = '127.0.0.1', user = 'yang', port = 1234, password = "05012014", dbname = 'map')
-sql = 'SELECT observation, utterID, who, ent FROM utterances WHERE ent IS NOT NULL'
+sql = 'SELECT observation, utterID, who, ent, topicID, inTopicID FROM utterances WHERE ent IS NOT NULL'
 df = dbGetQuery(conn, sql)
 
 summary(lmer(ent ~ utterID + (1|observation), df))
@@ -26,3 +26,13 @@ dt = data.table(df)
 setkey(dt, observation)
 dt_len = dt[, .(convLen = max(utterID)), by = observation]
 # the survey shows only 7 conversations have fewer than 100 sentences
+
+# check the number of topics
+dt_topic = dt[, .(topicNum = length(unique(topicID))), by = observation]
+
+
+## plot ent vs. inTopicID, grouped by who
+p1 = ggplot(subset(dt, inTopicID <= 10), aes(x = inTopicID, y = ent, group = who)) +
+    stat_summary(fun.y = mean, geom = 'line', aes(lty = who)) +
+    stat_summary(fun.data = mean_cl_boot, geom = 'ribbon', aes(fill = who), alpha = .5) +
+    scale_x_continuous(breaks = 1:10)
