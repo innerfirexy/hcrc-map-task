af07645214383aff4a3f7c8384aa78b81e20ae19
Yang Xu <innerfirexy@gmail.com>
2016-03-29 11:27:59 -0400

    correlation analysis

--- analysis.r
+++ analysis.r
@@ -110,3 +110,62 @@ dt.coef = dt.coef[dt2[, .(observation, pathdev)]]
 cor.test(dt.coef$coef1, dt.coef$pathdev)
 cor.test(dt.coef$coef2, dt.coef$pathdev)
 cor.test(dt.coef$coef1, dt.coef$coef2)
+
+
+
+## join dt and dt2
+dt3 = dt[dt2[, .(observation, pathdev)], nomatch = 0]
+
+# the correlation between overall entropy level and pathdev
+cor.test(dt3$ent, dt3$pathdev) # insig
+cor.test(dt3[topicRole == 'initiator', ent], dt3[topicRole == 'initiator', pathdev]) # r = 0.028, t = 1.999, p-value = 0.04566*
+cor.test(dt3[topicRole == 'responder', ent], dt3[topicRole == 'responder', pathdev]) # insig
+
+cor.test(dt3[who == 'f', ent], dt3[who == 'f', pathdev]) # insig
+cor.test(dt3[who == 'g', ent], dt3[who == 'g', pathdev]) # r = 0.029, t = 2.1358, p = 0.033*
+
+# tree depth
+cor.test(dt3$td, dt3$pathdev) # insig
+cor.test(dt3[topicRole == 'initiator', td], dt3[topicRole == 'initiator', pathdev]) # insig
+cor.test(dt3[topicRole == 'responder', td], dt3[topicRole == 'responder', pathdev]) # insig
+
+cor.test(dt3[who == 'f', td], dt3[who == 'f', pathdev]) # r = 0.030, t = 2.9702, p = 0.003**
+cor.test(dt3[who == 'g', td], dt3[who == 'g', pathdev]) # r = -0.023, t = -2.5842, p = 0.01**
+
+# branching factor
+cor.test(dt3[topicRole == 'initiator', bf], dt3[topicRole == 'initiator', pathdev]) # insig
+cor.test(dt3[topicRole == 'responder', bf], dt3[topicRole == 'responder', pathdev]) # r = 0.022, t = 2.1751, p = 0.03*
+
+cor.test(dt3[who == 'f', bf], dt3[who == 'f', pathdev]) # r = 0.028, t = 2.784, p = 0.005**
+cor.test(dt3[who == 'g', bf], dt3[who == 'g', pathdev]) # insig
+
+# normalized tree depth
+cor.test(dt3[topicRole == 'initiator', tdAdj], dt3[topicRole == 'initiator', pathdev]) # insig, r = 0.013, t = 1.442, p = 0.149
+cor.test(dt3[topicRole == 'responder', tdAdj], dt3[topicRole == 'responder', pathdev]) # insig
+
+cor.test(dt3[who == 'f', tdAdj], dt3[who == 'f', pathdev]) # r = 0.017, t = 1.685, p = 0.09, marginal
+cor.test(dt3[who == 'g', tdAdj], dt3[who == 'g', pathdev]) # insig
+
+# normalized branching factor
+cor.test(dt3[topicRole == 'initiator', bfAdj], dt3[topicRole == 'initiator', pathdev]) # insig
+cor.test(dt3[topicRole == 'responder', bfAdj], dt3[topicRole == 'responder', pathdev]) # insig
+
+cor.test(dt3[who == 'f', bfAdj], dt3[who == 'f', pathdev]) # insig
+cor.test(dt3[who == 'g', bfAdj], dt3[who == 'g', pathdev]) # insig
+
+# tokenNum
+cor.test(dt3$tokenNum, dt3$pathdev) # r = -0.013, t = -1.9654, p = 0.049*
+cor.test(dt3[topicRole == 'initiator', tokenNum], dt3[topicRole == 'initiator', pathdev]) # r = -0.021, t = -2.360, p = 0.018*
+cor.test(dt3[topicRole == 'responder', tokenNum], dt3[topicRole == 'responder', pathdev]) # insig
+
+cor.test(dt3[who == 'f', tokenNum], dt3[who == 'f', pathdev]) # r = 0.022, t = 2.213, p = 0.027*
+cor.test(dt3[who == 'g', tokenNum], dt3[who == 'g', pathdev]) # r = -0.028, t = -3.157, p = 0.002**
+
+
+## mixed models
+# centerize entropy, pathdev
+dt3[!is.na(ent), entc := ent / max(ent)][, pathdevc := pathdev / max(pathdev)]
+
+summary(lmer(pathdevc ~ entc + (1|observation), dt3))
+
+summary(lmer(pathdev ~ td + (1|observation), dt3))
